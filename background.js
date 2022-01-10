70860fc59ca0fdeb48098693bfa3c3cd7802f1cd
anshgupta203 <56402745+anshgupta203@users.noreply.github.com>
2022-01-10 16:43:20 +0530

    Initial commit

---
+++ background.js
@@ -0,0 +1,30 @@
+
+function MyImageClick(info, tab){
+    console.log("Clicked an image", info, tab);
+    
+    chrome.windows.create({
+        "url": "https://facebook.com/sharer.php?u=" + info.srcUrl + "&display=popup",
+        "type": "popup"
+    })
+}
+
+function MyQuoteClick(info, tab){
+    console.log("Clicked an image", info, tab);
+    
+    chrome.windows.create({
+        "url": "https://facebook.com/sharer.php?u=" + info.pageUrl + "&display=popup&quote=" + info.selectionText,
+        "type": "popup"
+    })
+}
+
+chrome.contextMenus.create({
+    "title": "Share Image",
+    "contexts": ["image"],
+    "onclick": MyImageClick
+})
+
+chrome.contextMenus.create({
+    "title": "Share Quote",
+    "contexts": ["selection"],
+    "onclick": MyQuoteClick
+})
